803d6bf596e47101a3fd6053f2115dbaaa44db67
Meredith Nolin <meredith.nolin9@etu.univ-lorraine.fr>
2022-01-06 11:02:19 +0100

    CT dev mobile

---
+++ src/helpers/FakeObjects.js
@@ -0,0 +1,22 @@
+const fakeObjects = [];
+
+fakeObjects.push(
+  {
+    id: 6454368,
+    name: "Object 1",
+  },
+  {
+    id: 5892342,
+    name: "Object 2",
+  },
+  {
+    id: 8964235,
+    name: "Object 3",
+  },
+  {
+    id: 6875634,
+    name: "Object 4",
+  }
+);
+
+export default fakeObjects;
